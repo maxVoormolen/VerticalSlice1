44a0639636f6eaedf6280e93d7ad563205fa3af1
maxVoormolen <33901303+maxVoormolen@users.noreply.github.com>
2018-11-28 11:02:27 +0100

    Add files via upload
    object rotator 1.0

---
+++ ObjectRotator.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotator : MonoBehaviour {
+    public float _sensitivity;
+    private Vector3 _mouseReference;
+    private Vector3 _mouseOffset;
+    private Vector3 _rotation;
+    public bool _isRotating;
+
+    void Start()
+    {
+        _sensitivity = 0.4f;
+        _rotation = Vector3.zero;
+    }
+
+    void Update()
+    {
+        if (_isRotating)
+        {
+            // offset
+            _mouseOffset = (Input.mousePosition - _mouseReference);
+
+            // apply rotation
+            _rotation.y = -(_mouseOffset.x + _mouseOffset.y) * _sensitivity;
+
+            // rotate
+            transform.Rotate(_rotation);
+
+            // store mouse
+            _mouseReference = Input.mousePosition;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        /*if (PlayerIsMoving == true)
+        {
+
+        }
+        else if (PlayerOnPlatform == true)
+        {
+
+        }
+        else
+        {
+            _isRotating = true;
+        }*/
+        _isRotating = true;
+
+        // store mouse
+        _mouseReference = Input.mousePosition;
+    }
+
+    void OnMouseUp()
+    {
+        _isRotating = false;
+    }
+}
